7692cef88fbed22cbafcbd4ce34ae35834c21a3a
TaylanALTUN <taylanaltun@gmail.com>
2021-01-25 20:56:12 +0300

    Add project files.

---
+++ Dictionary/MyDictionary.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+    class MyDictionary<KeyType, ValueType>
+    {
+        KeyType[] _keyArray;
+        KeyType[] _tempKeyArray;
+        ValueType[] _valueArray;
+        ValueType[] _tempValueArray;
+
+        public MyDictionary()
+        {
+            _keyArray = new KeyType[0];
+            _valueArray = new ValueType[0];
+        }
+
+        public void Add(KeyType key, ValueType value)
+        {
+                _tempKeyArray = _keyArray;
+
+                _keyArray = new KeyType[_keyArray.Length + 1];
+
+                for (int i = 0; i < _tempKeyArray.Length; i++)
+                {
+                    _keyArray[i] = _tempKeyArray[i];
+                }
+
+                _keyArray[_keyArray.Length - 1] = key;
+
+                _tempValueArray = _valueArray;
+
+                _valueArray = new ValueType[_valueArray.Length + 1];
+
+                for (int i = 0; i < _tempValueArray.Length; i++)
+                {
+                    _valueArray[i] = _tempValueArray[i];
+                }
+
+                _valueArray[_valueArray.Length - 1] = value;            
+        }
+
+        public int Count
+        {
+            get { return _keyArray.Length; }           
+        }
+
+        public KeyType[] Key
+        {
+            get { return _keyArray; }
+        }
+
+        public ValueType[] Value
+        {
+            get { return _valueArray; }
+        }
+
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1, "Value1");
+            myDictionary.Add(2, "Value2");
+            myDictionary.Add(3, "Value3");
+            myDictionary.Add(4, "Value4");
+            myDictionary.Add(5, "Value5");
+
+            ListMyDictionary(myDictionary);
+        }
+
+        static void ListMyDictionary(MyDictionary<int, string> myDictionary)
+        {
+            for (int i = 0; i < myDictionary.Count; i++)
+            {
+                Console.WriteLine("( " + myDictionary.Key[i] + " , " + myDictionary.Value[i] + " )");
+            }
+        }
+    }
+}
